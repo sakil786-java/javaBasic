7a9d617854cc35631c381f0c67d459106667a193
sakil786-java <sakilahammadlaskar786@gmail.com>
2025-01-16 14:28:51 +0530

    Just Check

--- com/company/Main.java
+++ com/company/Main.java
@@ -4,8 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-
         System.out.println("Local Changes");
         System.out.println("");
+        System.out.println("Doing for commit");
     }
 }
